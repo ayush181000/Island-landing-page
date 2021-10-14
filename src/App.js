3e0c5fe664d4618d11180c06009b546e31c06ab7
ayush181000 <ayush181000@gmail.com>
2021-10-14 07:09:24 +0530

    Header ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from './components/Header';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + `/assets/bg.jpg`})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import SortIcon from '@mui/icons-material/Sort';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Toolbar from '@mui/material/Toolbar';
+import Collapse from '@mui/material/Collapse';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    height: '100vh',
+    fontFamily: 'Nunito',
+  },
+  appbar: {
+    background: 'none',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '4.5rem',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  icon: {
+    color: '#fff',
+    fontSize: '2rem',
+  },
+  colorText: {
+    color: '#5AFF3D',
+  },
+  goDown: {
+    color: '#5AFF3D',
+    fontSize: 'large',
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(true);
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            My<span className={classes.colorText}>Island</span>
+          </h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Collapse
+        in={checked}
+        {...(true ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br /> My{' '}
+            <span className={classes.colorText}>Island.</span>
+          </h1>
+          <IconButton>
+            <ExpandMoreIcon className={classes.goDown} />
+          </IconButton>
+        </div>
+      </Collapse>
+    </div>
+  );
+}
