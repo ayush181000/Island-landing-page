dd2c553d2f7b6853e34b0e07b8240d0258e031a2
ayush181000 <ayush181000@gmail.com>
2021-11-29 15:17:50 +0530

    Updates

---
+++ precache-manifest.a54f238412a5b5c2c5a8a683283a6600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee6bef51889fc32719ed23b4315dd08",
+    "url": "/Island-website-react/index.html"
+  },
+  {
+    "revision": "d6ffb50c0d6d7e9acc37",
+    "url": "/Island-website-react/static/js/2.e9319a6d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Island-website-react/static/js/2.e9319a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7f7c40d6d833b4cd2d",
+    "url": "/Island-website-react/static/js/main.5832e9d3.chunk.js"
+  },
+  {
+    "revision": "26aad2bb9217f0f82cf3",
+    "url": "/Island-website-react/static/js/runtime-main.cf057c7a.js"
+  }
+]);
